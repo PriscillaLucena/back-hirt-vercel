2af5daf5521a56f53043e7f6c415bec07cece7ed
PriscillaLucena <prilucenaoliveira@gmail.com>
2023-03-10 11:42:47 -0300

    vercel version

--- back/src/Data/BaseDatabase.ts
+++ back/src/Data/BaseDatabase.ts
@@ -11,8 +11,10 @@ export default abstract class BaseDatabase {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_SCHEMA,
-            port: 3306,
+            port: 3006,
             multipleStatements: true
         }
     })
 }
+
+console.log(process.env.PORT)
