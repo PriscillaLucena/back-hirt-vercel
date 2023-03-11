a74a8f5453e1bed1e57f4a54328e76b0a3d8564f
PriscillaLucena <prilucenaoliveira@gmail.com>
2023-03-11 00:39:09 -0300

    package json com noso script start

--- hirt-project/src/tables.sql
+++ hirt-project/src/tables.sql
@@ -1,12 +1,12 @@
 -- SQLBook: Code
 -- Active: 1673741533992@@35.226.146.116@3306@Hopper-4314151-priscilla-ramos
-INSERT INTO nome_obra (id, qty_andares, qty_total_ap)
-VALUES ("12EIE12", 2, 3);
+-- INSERT INTO nome_obra (id, qty_andares, qty_total_ap)
+-- VALUES ("12EIE12", 2, 3);
 
-INSERT INTO apartamentos (id, numero_ap, andar, limpeza_completa, data, foto)
-VALUES ("34eiei12", 101, 1, false, 2023/01/12, ""),
-("54IEIE23", 102, 1, true, 2023/01/12, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM9kQZpCulhigSwNVjT8hzhKrt-gN0IpR-km2TxGEHlQ&s"),
-("78ioio89", 203, 2, false, 2023/01/12, "");
+-- INSERT INTO apartamentos (id, numero_ap, andar, limpeza_completa, data, foto)
+-- VALUES ("34eiei12", 101, 1, false, 2023/01/12, ""),
+-- ("54IEIE23", 102, 1, true, 2023/01/12, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM9kQZpCulhigSwNVjT8hzhKrt-gN0IpR-km2TxGEHlQ&s"),
+-- ("78ioio89", 203, 2, false, 2023/01/12, "");
 
 -- CREATE TABLE IF NOT EXISTS Novas_obras( 
 -- id VARCHAR(255) PRIMARY KEY NOT NULL,
